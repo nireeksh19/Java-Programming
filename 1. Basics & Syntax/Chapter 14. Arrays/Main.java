370255196b085eaa19375f5804dd0c0deed5a389
nireeksh-shetty <niree999@gmail.com>
2022-06-13 23:40:32 +0530

    basics and syntax

---
+++ 1. Basics & Syntax/Chapter 14. Arrays/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        // int[] num ; //Initialized array
+        int[] num ={1,5,6,7,9};
+        System.out.println(num[0]); //1
+
+        System.out.println("length og array: "+num.length);
+
+        // multi dimensional
+        int[][] mynum = {{1,2,3},{4,5,6}};
+        System.out.println(mynum[1][2]); // outputs 6
+    }
+}
