63d2dcb09d45fd5b23244717c38f950c0c30d63b
nireeksh-shetty <niree999@gmail.com>
2022-06-13 15:11:31 +0530

    update

---
+++ 4. Data Types/4.1 Numbers/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        short myNum = 5000;
+        System.out.println(myNum);
+        int myNum1 = 100000;
+        System.out.println(myNum1);
+        long myNum2 = 15000000000L;
+        System.out.println(myNum2);
+        float myNum3 = 5.75f;
+        System.out.println(myNum3);
+        double myNum4 = 19.99d;
+        System.out.println(myNum4);
+    }
+
+}
