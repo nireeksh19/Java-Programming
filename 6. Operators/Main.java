2f73bc75f95cd71c406b736562e13b769ed433e3
nireeksh-shetty <niree999@gmail.com>
2022-06-13 16:05:26 +0530

    update

---
+++ 6. Operators/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int x = 3, y = 2; //assignment op
+
+        System.out.println(x+y); //arithmetic operator + - * %  x++ x--
+
+        System.out.println(x+=20);//addition assignment op.. x=3+20 = 23
+
+        System.out.println(x>y);//comparision op == != < > <= etc returns boolean ans
+
+        System.out.println(y > 3 && y < 10);//logical op
+        
+    }
+}
