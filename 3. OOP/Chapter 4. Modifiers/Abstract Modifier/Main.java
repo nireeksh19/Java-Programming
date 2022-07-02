accfbca2dd7d73f099d99f2ad65c01e9988055e0
nireeksh-shetty <niree999@gmail.com>
2022-07-02 17:07:28 +0530

    updates

---
+++ 3. OOP/Chapter 4. Modifiers/Abstract Modifier/Main.java
@@ -0,0 +1,15 @@
+// An abstract method belongs to an abstract class, and it does not have a body. The body is provided by the subclass:
+// abstract class
+abstract class Main {
+    public String fname = "John";
+    public int age = 24;
+    public abstract void study(); // abstract method
+  }
+  
+  // Subclass (inherit from Main)
+  class Student extends Main {
+    public int graduationYear = 2018;
+    public void study() { // the body of the abstract method is provided here
+      System.out.println("Studying all day long");
+    }
+  }
