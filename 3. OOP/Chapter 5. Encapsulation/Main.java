920ad28258f8948c6ab5906758b2f482e87df1c1
nireeksh-shetty <niree999@gmail.com>
2022-07-02 17:57:46 +0530

    updates

---
+++ 3. OOP/Chapter 5. Encapsulation/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      Person myObj = new Person();
+      myObj.setName("Nireeksh Shetty");
+      System.out.println(myObj.getName());
+    }
+  }
+  
---
+++ 3. OOP/Chapter 6. Inheritence/Car.java
@@ -0,0 +1,19 @@
+class Vehicle {
+  protected String brand = "Ford";
+  public void honk() {
+    System.out.println("Tuut, tuut!");
+  }
+}
+
+class Car extends Vehicle {
+  private String modelName = "Mustang";
+  public static void main(String[] args) {
+    Car myFastCar = new Car();
+    myFastCar.honk();
+    System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+  }
+}
+
+// subclass (child) - the class that inherits from another class
+// superclass (parent) - the class being inherited from
+// To inherit from a class, use the extends keyword.
